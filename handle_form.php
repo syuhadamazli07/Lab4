bd4d99474f9c82da634497c053d4eadb16c62a43
syuhadamazli07 <syuhadamazli07@gmail.com>
2022-05-13 13:46:25 +0800

    commit

--- handle_form.php
+++ handle_form.php
@@ -5,10 +5,14 @@
     <body>
     <?php #Script handle form 
 
-    echo "<p> Thank you, <b>".$_POST["name"].
-    "</b> for the following comments: <br/>".
-    $_POST ["comments"]."<p>We will reply to you at <i>".
-    $_POST ["email"]. "</i></p>";
+    if (strlen($_POST["name"]) >0) {
+        $_POST["name"] = $_POST["name"];
+    }
+    else{
+        $_POST["name"] = null;
+
+        echo '<p><b> You Forgot to Enter Your Name! </b></p>';
+    }
 
     ?>
     </body>
