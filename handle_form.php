555bfc5258ea738c40e431957a6a3519020b6425
syuhadamazli07 <syuhadamazli07@gmail.com>
2022-05-13 14:34:30 +0800

    commit

--- handle_form.php
+++ handle_form.php
@@ -10,7 +10,6 @@
     }
     else{
         $_POST["name"] = null;
-
         echo '<p><b> You Forgot to Enter Your Name! </b></p>';
     }
 
@@ -19,7 +18,6 @@
     }
     else{
         $_POST["comments"]= null;
-
         echo '<p><b> You Forgot to Enter Your Comments! </b></p>';
     }
     if(!(strlen($_POST["email"]) >0)) {
@@ -29,9 +27,23 @@
 
     if(isset($_POST["gender"])){
         if($_POST["gender"] == 'M'){
+            $message = '<b><p>Good Day, Sir </b></p>';
+        }
+        if($_POST["gender"] == 'F'){
             $message = '<b><p>Good Day, Madam </b></p>';
         }
+    }else{
+            $_POST["gender"]= null;
+            echo '<p><b> You Forgot to Choose Your Gender! </b></p>';
     }
+
+    if ($_POST["name"] && $_POST["comments"] && $_POST["email"] && $_POST["comments"] && $_POST["gender"]) {
+        echo "Thank you," .$_POST["name"]. "for the following comments: <br /> <tt>". 
+        $_POST["comments"]. " </tt> <p> We will reply to you at <i>". $_POST["email"].
+        "</i></p>";
+        echo $message;
+    }
+
     ?>
     </body>
 </html>
