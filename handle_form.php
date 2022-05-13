005107ee54d66f0b91ccec10aeca325f8855ac7e
syuhadamazli07 <syuhadamazli07@gmail.com>
2022-05-13 14:20:22 +0800

    commit

--- handle_form.php
+++ handle_form.php
@@ -14,6 +14,24 @@
         echo '<p><b> You Forgot to Enter Your Name! </b></p>';
     }
 
+    if (strlen ($_POST ["comments"]) >0) {
+        $_POST["comments"] = $_POST["comments"];
+    }
+    else{
+        $_POST["comments"]= null;
+
+        echo '<p><b> You Forgot to Enter Your Comments! </b></p>';
+    }
+    if(!(strlen($_POST["email"]) >0)) {
+    $_POST["email"] = null; 
+    echo '<p><b> You Forgot to Enter Your Emails! </b></p>';
+    } 
+
+    if(isset($_POST["gender"])){
+        if($_POST["gender"] == 'M'){
+            $message = '<b><p>Good Day, Madam </b></p>';
+        }
+    }
     ?>
     </body>
 </html>
